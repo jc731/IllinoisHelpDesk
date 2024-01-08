1c852de092e6d337ed5393ce7de603e545f90983
Jon Cagle <joncagle@unisyntechnologies.com>
2024-01-07 20:55:45 -0600

    updates, content, and hopefully some fun config things that are working correctly now.

--- src/navigation.js
+++ src/navigation.js
@@ -1,20 +1,24 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
-  // links: [
-  //   {
-  //     text: 'Helpdesk Services',
-  //     links: [
-  //       {
-  //         text: 'Tier 1 Support',
-  //         href: getPermalink('/tier1'),
-  //       },
-  //       // {
-  //       //   text: 'Tier 2 Support',
-  //       //   href: getPermalink('/tier2'),
-  //       // },
-  //     ],
-  //   },
+  links: [
+    {
+      text: 'Helpdesk Services',
+      links: [
+        {
+          text: 'Tier 1 Support',
+          href: getPermalink('/tier1'),
+        },
+        // {
+        //   text: 'Tier 2 Support',
+        //   href: getPermalink('/tier2'),
+        // },
+      ],
+    },
+    {
+      text: 'Knowledge Base',
+      href: getPermalink('/knowledgebase'),
+    }
   //   // {
   //   //   text: 'IT Project Support',
   //   //   href: '/projectmanagement'
@@ -23,7 +27,7 @@ export const headerData = {
   //   //   text: 'Start Ups',
   //   //   href: '/startups',
   //   // },
-  // ],
+  ],
   actions: [{ text: 'Contact Us', href: '/contact' }],
 }
 
@@ -62,17 +66,23 @@ export const footerData = {
     //     { text: 'Shop', href: '#' },
     //   ],
     // },
+    {
+      title: 'Knowledge Base',
+      links: [
+        { text: 'Tips, Tricks, and Guides', href: '/knowledgebase' },
+      ],
+    }
   ],
   secondaryLinks: [
     // { text: 'Terms', href: getPermalink('/terms') },
     // { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    // { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: '#' },
-    // { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: '#' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
     Powered by <a class="text-orange-500 hover:underline" href="https://unisyntechnologies.com"> UniSyn Technologies</a> · All rights reserved.
